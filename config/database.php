b67bb66866092c73fb85c0750f09f0f56970cd72
nicoleabril <nicole.abrilc@ucuenca.edu.ec>
2025-11-06 23:20:58 -0500

    Cambios para migracion

--- config/database.php
+++ config/database.php
@@ -66,8 +66,8 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'host' => env('DB_HOST', 'nozomi.proxy.rlwy.net'),
+            'port' => env('DB_PORT', '39714'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
