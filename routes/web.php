cbf691e64c9c2f43cf8e39bdee2ffaf2ed5466a4
nicoleabril <nicole.abrilc@ucuenca.edu.ec>
2025-11-06 23:55:38 -0500

    Arreglo de rutas en API

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-    ->name('login');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->name('logout');
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
